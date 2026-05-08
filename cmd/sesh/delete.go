56447b059e529bf83597b1471b7d6751a9214d2d
Ian Duggan <ian@ianduggan.net>
2026-05-08 16:22:07 -0700

    cli: sesh delete <name> [-y]

---
+++ cmd/sesh/delete.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/ijcd/sesh/internal/config"
+)
+
+func newDeleteCmd() *cobra.Command {
+	var yes bool
+	cmd := &cobra.Command{
+		Use:   "delete <name>",
+		Short: "Delete a project file",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, err := config.ResolveProjectPath(args[0])
+			if err != nil {
+				return err
+			}
+			if _, err := os.Stat(path); err != nil {
+				return fmt.Errorf("not found: %s", path)
+			}
+			if !yes {
+				fmt.Printf("delete %s? [y/N] ", path)
+				line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+				if strings.TrimSpace(strings.ToLower(line)) != "y" {
+					fmt.Println("aborted")
+					return nil
+				}
+			}
+			if err := os.Remove(path); err != nil {
+				return err
+			}
+			fmt.Printf("deleted %s\n", path)
+			return nil
+		},
+	}
+	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip the confirmation prompt")
+	return cmd
+}
--- cmd/sesh/stubs.go
+++ cmd/sesh/stubs.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ijcd/sesh/internal/engine"
 )
 
-func newDeleteCmd() *cobra.Command              { return &cobra.Command{Use: "delete", Hidden: true} }
 func newDebugCmd(*engine.Engine) *cobra.Command { return &cobra.Command{Use: "debug", Hidden: true} }
 func newCaptureCmd(*engine.Engine) *cobra.Command {
 	return &cobra.Command{Use: "capture", Hidden: true}
