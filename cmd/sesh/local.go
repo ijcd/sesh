58e4ec38905aff8e471e87eabcdfd8d62a23af4e
Ian Duggan <ian@ianduggan.net>
2026-05-08 16:23:42 -0700

    cli: sesh local (./.sesh.yml)

---
+++ cmd/sesh/local.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"github.com/ijcd/sesh/internal/config"
+	"github.com/ijcd/sesh/internal/engine"
+)
+
+func newLocalCmd(e *engine.Engine) *cobra.Command {
+	var force bool
+	cmd := &cobra.Command{
+		Use:   "local",
+		Short: "Run ./.sesh.yml from the current directory",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			cwd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			path := filepath.Join(cwd, ".sesh.yml")
+			if _, err := os.Stat(path); err != nil {
+				return fmt.Errorf("no .sesh.yml in %s", cwd)
+			}
+			p, err := config.LoadFromPath(path, e.Drivers(), nil)
+			if err != nil {
+				return err
+			}
+			return e.Up(context.Background(), p, force)
+		},
+	}
+	cmd.Flags().BoolVar(&force, "force", false, "Down + Up if a session already exists")
+	return cmd
+}
--- cmd/sesh/stubs.go
+++ cmd/sesh/stubs.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ijcd/sesh/internal/engine"
 )
 
-func newLocalCmd(*engine.Engine) *cobra.Command { return &cobra.Command{Use: "local", Hidden: true} }
 func newValidateCmd(*engine.Engine) *cobra.Command {
 	return &cobra.Command{Use: "validate", Hidden: true}
 }
